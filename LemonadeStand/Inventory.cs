e3072a32ee046ccfca5841290a79c70ca9fe8fdf
JasonRoyalHart <jason@jasonrhart.com>
2016-12-01 15:53:02 -0600

    First commit.

---
+++ LemonadeStand/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Inventory
+    {
+        int lemons;
+        int sugar;
+        int cups;
+        int ice;
+
+        public Inventory()
+        {
+            lemons = 0;
+            sugar = 0;
+            cups = 0;
+            ice = 0;
+        }
+    }
+}
